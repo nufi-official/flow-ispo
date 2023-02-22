ccbd5c0f87463b2893d21da67b019ab9ac57cd23
AndrejZelonka <andrej.zelonka@gmail.com>
2023-02-22 05:37:08 +0100

    add babel plugin import for mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "../theme";
 import "../styles/globals.css";
