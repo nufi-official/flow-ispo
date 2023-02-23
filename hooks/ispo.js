7de88442abd0646beb8d8f3af84642c7ae688ec0
refi93 <rafael.korbas@gmail.com>
2023-02-23 14:29:54 +0100

    Mock useAccountIspos

--- hooks/ispo.js
+++ hooks/ispo.js
@@ -4,31 +4,66 @@ import getAccountISPOs from '../cadence/web/scripts/getAccountISPOs.cdc'
 import * as fcl from '@onflow/fcl'
 
 
+const fetchIspos = async () => {
+    let res
+    try {
+        res = (await fcl.query({
+            cadence: getIspoInfos,
+            args: (arg, t) => []
+        })).map((ispo) => (
+            {...ispo, createdAt: new Date(Number(ispo.createdAt) * 1000)}
+        ))
+    } catch(e) {
+        // Likely need to mint first to create capability if this fails
+        res = []
+    } finally {
+        return res
+    }
+}
+
 export function useIspos() {
     const [ispos, setIspos] = useState([])
 
-    const fetchIspos = async () => {
-        let res
-        try {
-            res = (await fcl.query({
-                cadence: getIspoInfos,
-                args: (arg, t) => []
-            })).map((ispo) => (
-                {...ispo, createdAt: new Date(Number(ispo.createdAt) * 1000)}
-            ))
-        } catch(e) {
-            // Likely need to mint first to create capability if this fails
-            res = []
-        } finally {
-            setIspos(res)
-        }
+    const fetch = async () => {
+        setIspos(await fetchIspos())
     }
 
     useEffect(() => {
-        fetchIspos()
+        fetch()
     }, [])
 
     return ispos
+}
+
+const mockIspo = {
+    "ispo": {
+        "id": "44",
+        "name": "Mock ISPO",
+        "rewardTokenBalance": "47000.00000000",
+        "rewardTokenMetadata": {
+            "rewardTokenVaultStoragePath": {
+                "domain": "storage",
+                "identifier": "ispoExampleRewardTokenVault"
+            },
+            "rewardTokenReceiverPublicPath": {
+                "domain": "public",
+                "identifier": "ispoExampleRewardTokenReceiver"
+            },
+            "rewardTokenBalancePublicPath": {
+                "domain": "public",
+                "identifier": "ispoExampleRewardTokenBalance"
+            },
+            "totalRewardTokenAmount": "47000.00000000"
+        },
+        "epochStart": "0",
+        "epochEnd": "12",
+        "delegationsCount": "0",
+        "delegatedFlowBalance": "0.00000000",
+        "flowRewardsBalance": "0.00000000",
+        "createdAt": "2023-02-23T13:24:42.000Z"
+    },
+    "delegatedFlowBalance": "123456.0",
+    "rewardTokenBalance": "4747"
 }
 
 export function useAccountIspos(address) {
@@ -42,11 +77,12 @@ export function useAccountIspos(address) {
                 args: (arg, t) => [
                     arg(address, t.Address)
                 ]
-            }) : null
+            }) : []
         } catch(e) {
             // Likely need to mint first to create capability if this fails
-            res = null
+            res = []
         } finally {
+            res = !res?.length ? [mockIspo] : []
             setIspos(res)
         }
     }
--- pages/create/index.js
+++ pages/create/index.js
@@ -1,6 +1,7 @@
 import useCurrentUser from "../../hooks/useCurrentUser";
 import { CreateIspoForm } from "../../components/CreateIspoForm";
 import { Container } from "@mui/material";
+import { useAccountIspos } from "../../hooks/ispo";
 
 export default function CreateIspoPage() {
   const { addr } = useCurrentUser();
